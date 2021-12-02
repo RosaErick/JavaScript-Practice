95d4905177c13849c000588b5303f0cf1dca80bb
Erick Rosa <erickpmotta@gmail.com>
2021-12-01 23:43:41 -0300

    new project changes

--- Drum-Kit-JS/assets/app.js
+++ Drum-Kit-JS/assets/app.js
@@ -1,6 +1,6 @@
-window.addEventListener('keydown', function (e) {
-    
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+function playSound(e){
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     
     if (!audio) return; //faz a função não rodar toda de uma
@@ -8,10 +8,15 @@ window.addEventListener('keydown', function (e) {
     audio.currentTime = 0; 
     audio.play();
     key.classList.add('playing');
+}
 
-});
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
 
 const keys = document.querySelectorAll('.key');
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 console.log(keys);
+window.addEventListener('keydown', playSound);
