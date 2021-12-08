59b4cd3954802e51f76daa6896f27efcfc3202be
Erick Rosa <erickpmotta@gmail.com>
2021-12-07 21:49:19 -0300

    finish array-cardio exercise

--- Array-Cardio/app.js
+++ Array-Cardio/app.js
@@ -53,16 +53,71 @@ const ordered = inventors.sort(function (a, b) {
 console.table(ordered);
     // Array.prototype.reduce()
     // 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+
+}, 0);
+
+console.log(totalYears);
+
 
     // 5. Sort the inventors by years lived
+
+const oldest = inventors.sort(function (a, b) {
+    const lastGuy = a.passed - a.year;
+    const nextGuy = a.passed - a.year;
+
+    if (lastGuy > nextGuy) {
+        return -1;
+
+    }
+    else {
+        return 1;
+
+    }
+
+});
+
+console.table(oldest);
+
 
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+/*const category = document.querySelector('.mw-category');
+const links = [...category.querySelectorAll('a')];
 
+const de = links
+    .map(link => link.textContent)
+    .filter(streetName => streetName.includes('de'));
+*/
     // 7. sort Exercise
     // Sort the people alphabetically by last name
 
+const alpha = people.sort((lastOne, nextOne) =>{
+    const [last, aFirst] = lastOne.split(', ');
+      const [bLast, bFirst] = lastOne.split(', ');
+    
+    
+    return last > bLast ? 1 : -1;
+
+})
+    
+console.log(alpha);
+
     // 8. Reduce Exercise
     // Sum up the instances of each of these
-    const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+    
+
+const transportation = data.reduce(function (obj, item) {
+    if (!obj[item]){
+        obj[item] = 0;
+    }
+
+    obj[item] ++
+    return obj;
+
+}, {});
+
+console.log(transportation);
