e268565a3f0369aa8abef27c4520f957895282cf
Erick Rosa <erickpmotta@gmail.com>
2021-12-20 23:37:27 -0300

    finish js

--- Ajax-type-ahead/app.js
+++ Ajax-type-ahead/app.js
@@ -18,7 +18,26 @@ function findMatches(wordToMatch, cities) {
 }
 
 function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span> `)
+        const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span> `)
+        return `
+        <li>
+        <span class="name">${cityName},${stateName}</span>
+        <span class="population">${place.population}</span>
+        </li>
+        `;
 
-    
+
+    })
+    suggestions.innerHTML = html;
 
 }
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
