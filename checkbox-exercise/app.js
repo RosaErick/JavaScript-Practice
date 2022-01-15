7cdc6e01e8ca9592be2834f70060b2446906b397
Erick Rosa <erickpmotta@gmail.com>
2022-01-14 22:04:26 -0300

    finish project

--- checkbox-exercise/app.js
+++ checkbox-exercise/app.js
@@ -7,8 +7,22 @@ let lastChecked;
 
 function handleCheck(e) {
 
+    let inBetween = false;
+
     if (e.shiftKey & this.checked) {
-    
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                    
+                inBetween = !inBetween;
+            }
+        
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+
+        
+        })
 
 }
 
